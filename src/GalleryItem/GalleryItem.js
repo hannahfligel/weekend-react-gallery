7f622741bb39337f75630e4e950c2a4554ffbc32
Hannah Fligel <fligelhannah@gmail.com>
2021-10-28 17:59:25 -0500

    toggle between image and text created

--- src/GalleryItem/GalleryItem.js
+++ src/GalleryItem/GalleryItem.js
@@ -2,10 +2,20 @@ import { useState } from "react" ;
 
 function GalleryItem ( props ){
     //const[name, setName]= useState(null);
+    const[show, setShow]=useState( true );
+
+    const toggleImage = () =>{
+        setShow (!show);
+    }
+
     return(
         <div>
-            <h1>GalleryItem</h1>
-            <img width="500px" src={props.image.path}></img>
+            {
+                show?
+                // <h1>GalleryItem</h1>
+                <img width="500px" src={ props.image.path } onClick={ toggleImage }></img>: //true
+                <h1 onClick={ toggleImage }>{props.image.description}</h1>
+            }
         </div>
     )
 }
